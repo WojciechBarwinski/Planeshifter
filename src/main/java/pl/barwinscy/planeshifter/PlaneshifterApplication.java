93afc673a45ad6ea8d82afc81692dbe473f77a8d
thaaz <thaazar@gmail.com>
2020-09-13 09:57:05 +0200

    Initial commit

---
+++ src/main/java/pl/barwinscy/planeshifter/PlaneshifterApplication.java
@@ -0,0 +1,13 @@
+package pl.barwinscy.planeshifter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaneshifterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaneshifterApplication.class, args);
+    }
+
+}
